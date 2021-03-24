37e6de294820e2872b24658c0a5473c416f6b209
Jason Lee <huacnlee@gmail.com>
2021-03-24 11:03:00 +0800

    Fix for Crystal 1.0

--- spec/markdown_filter_spec.cr
+++ spec/markdown_filter_spec.cr
@@ -2,7 +2,7 @@ require "./spec_helper"
 
 private def assert_render(pipeline, input, output, file = __FILE__, line = __LINE__)
   it "call #{input.inspect}", file, line do
-    pipeline.call(input).should eq(output), file, line
+    pipeline.call(input).should eq(output)
   end
 end
 
--- spec/spec_helper.cr
+++ spec/spec_helper.cr
@@ -3,6 +3,6 @@ require "../src/html-pipeline"
 
 def assert_render(pipeline, input, output, file = __FILE__, line = __LINE__)
   it "call #{input.inspect}", file, line do
-    pipeline.call(input).should eq(output), file, line
+    pipeline.call(input).should eq(output)
   end
 end
